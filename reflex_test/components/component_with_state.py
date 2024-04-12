184235e47016055cec1d9710cdce13b6cd477fc5
Tim Child <timjchild@gmail.com>
2024-04-11 18:13:51 -0700

    Example of rx.ComponentState

--- reflex_test/components/component_with_state.py
+++ reflex_test/components/component_with_state.py
@@ -3,17 +3,23 @@ import random
 import reflex as rx
 
 
-class ComponentState(rx.State):
+class ComponentWithState(rx.ComponentState):
     text: str = "Hello, World!"
 
     def update(self):
-        self.text = random.choice(["Hello, Universe!", "Hello, you!", "Hello, Galaxy!", "Hello, Multiverse!"])
+        self.text = random.choice(
+            ["Hello, Universe!", "Hello, you!", "Hello, Galaxy!", "Hello, Multiverse!"]
+        )
+
+    @classmethod
+    def get_component(cls, *children, **props) -> rx.Component:
+        return rx.vstack(
+            rx.text(cls.text),
+            rx.button("Change State", on_click=cls.update),
+            *children,
+            **props,
+        )
 
 
-def layout() -> rx.Component:
-    return rx.box(
-        rx.text("Hello, World!"),
-        rx.text(ComponentState.text),
-        rx.button("Change State",
-                  on_click=ComponentState.update),
-    )
+
+component_with_state = ComponentWithState.create
