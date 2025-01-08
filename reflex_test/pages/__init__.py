ea6c9e7d389ea0c199e83896dc4ad278935b92ca
Tim Child <timjchild@gmail.com>
2025-01-07 18:32:48 -0800

    add duplicate tabs issue

--- reflex_test/pages/__init__.py
+++ reflex_test/pages/__init__.py
@@ -40,6 +40,7 @@ from .counter_example import index as counter_example
 from .markdown_with_copy import index as markdown_with_copy
 from .multi_checkbox import index as multi_checkbox
 from .textarea_expand_script import index as textarea_expand
+from .duplicate_tabs_issue import index as duplicate_tabs_issue
 
 
 # from .match_in_fstring_issue import index as match_in_fstring_issue
@@ -52,6 +53,7 @@ __all__ = [
     # "audio_record",
     "audio_recorder_polyfill",
     # "carousel",
+    "duplicate_tabs_issue",
     "scroll_area_width_issue",
     "component_with_state",
     "async_background_testing",
---
+++ reflex_test/pages/duplicate_tabs_issue.py
@@ -0,0 +1,35 @@
+import asyncio
+import reflex as rx
+
+
+class State(rx.State):
+    state_value: str = "Initial value"
+
+    @rx.event(background=True)
+    async def do_something(self):
+        async with self:
+            self.state_value = "Initial change"
+        await asyncio.sleep(2)
+        async with self:
+            self.state_value = "Final change"
+
+
+@template(route="/duplicate_tabs_issue", title="Duplicate tabs issue")
+# @rx.page(route="/duplicate_tabs_issue", title="Duplicate tabs issue")
+def index() -> rx.Component:
+    return rx.container(
+        rx.vstack(
+            rx.markdown("Minimal example to demonstrate issues with duplicated tab in Chrome."),
+            rx.markdown("`state.router.session.client_token` is not unique per tab when tab."),
+            rx.text(f"state.router.session.client_token: {rx.State.router.session.client_token}"),
+            rx.markdown("However, `state.router.session.session_id` does remain unique per tab."),
+            rx.text(f"state.router.session.session_id: {rx.State.router.session.session_id}"),
+            rx.divider(),
+            rx.text("Somehow the tabs interfere with each other during background tasks."),
+            rx.text(
+                "Expect to see 'Initial change' followed by 'Final change' after 2 seconds. But that doesn't happen if the button is clicked in the other tab before the first event finishes."
+            ),
+            rx.button("Do something", on_click=State.do_something),
+            rx.text(f"State.state_value: {State.state_value}"),
+        )
+    )
